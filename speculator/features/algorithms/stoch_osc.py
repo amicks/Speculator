ad112952d9066c492b875b5243812f9e811384af
Allston Mickey <allston.mickey@gmail.com>
2017-09-04 18:53:41 -0700

    Add implementations package

---
+++ speculator/features/algorithms/stoch_osc.py
@@ -0,0 +1,15 @@
+"""
+Stochastic Oscillator:
+%K = 100 * (C - L(t)) / (H14 - L(t))
+    such that,
+        C = Current closing Price
+        L(t) = Lowest Low over some duration t
+        H(t) = Highest High over some duration t
+          * t is usually 14 days
+
+%K follows the speed/momentum of a price in a market
+"""
+def stoch_oscillator(closing, low, high):
+    return 100 * (closing - low) / (high - low)
+
+
---
+++ speculator/features/implementations/rsi.py
@@ -0,0 +1,54 @@
+from speculator.features.algorithms import rsi as rsi_alg
+from speculator.utils import date
+from speculator.utils import poloniex
+
+def get_rsi_poloniex(year=None, month=None, day=None,
+                     direction='last', unit='day', count=14,
+                     period=86400, currency_pair='USDT_BTC'):
+    """
+    Gets RSI of a currency pair from Poloniex.com exchange
+
+    year: 1 <= year <= 9999, Poloniex market on year
+        type: integer
+    month: 1 <= month <= 12, Poloniex market on month
+        type: integer
+    day: 1 <= day <= 31, Poloniex market on day
+        type: integer
+    direction: 'last' (move backwards in time) or
+               'next' (move forwards in time)
+        type: string
+    unit: 'day', 'week', 'month', 'year' (Poloniex doesn't accept smaller times)
+        type: string
+    num_shifts: Number of shifts by unit in direction
+        type: integer
+    period: Candlestick period in seconds.
+            Valid values: 300, 900, 1800, 7200, 14400, 86400
+        type: int
+    
+    currency_pair: Currency symbols to compare
+        type: string
+
+    return: Momentum indicator of a market measuring
+            the speed and change of price movements
+        type: float
+    """
+    if year or month or day: # Date is specified
+        if not year:
+            year = 2017
+        if not month:
+            month = 1
+        if not day:
+            day = 1
+        cur_delorean = date.date_to_delorean(year, month, day)
+    else: # Date is not specified, get current date
+        count += 1 # Get another date because market is still open
+        direction = 'last' # Can't get future prices, must go back
+        cur_delorean = date.now_delorean()
+    
+    cur_epoch = int(cur_delorean.epoch)
+    prev_epoch = date.shift_epoch(cur_delorean, direction, unit, count) 
+
+    json = poloniex.chart_json(prev_epoch, cur_epoch, period, currency_pair)
+    changes = poloniex.get_gains_losses(poloniex.parse_changes(json))
+    return rsi_alg.rs_idx(changes['gains'], changes['losses'])
+
