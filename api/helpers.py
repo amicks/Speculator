d5b22448d7574b4784ccc30e7031e485ebda21be
Allston Mickey <allston.mickey@gmail.com>
2017-12-27 00:48:05 -0500

    Implement private GET/POST for market dates

--- api/helpers.py
+++ api/helpers.py
@@ -1,6 +1,17 @@
+from abc import ABC
 from flask import abort
 from flask_sqlalchemy import SQLAlchemy
 from functools import wraps
+
+class HTTP_CODES(ABC):
+    """ Constants for HTTP status codes """
+    BAD_REQUEST = 400
+    UNAUTHORIZED = 401
+    NOT_FOUND = 404
+    UNPROCESSABLE_ENTITY = 422
+
+def query_to_dict(query):
+    return dict((col, getattr(query, col)) for col in query.__table__.columns.keys())
 
 def validate_db(sqlalchemy_bind, is_enabled=True):
     """ Checks if a DB is authorized and responding before executing the function """
@@ -17,6 +28,6 @@ def validate_db(sqlalchemy_bind, is_enabled=True):
             if is_enabled and is_db_responsive():
                 return func(*args, **kwargs)
             else:
-                abort(401)
+                abort(HTTP_CODES.UNAUTHORIZED)
         return wrapper
     return decorator
--- api/resources/market.py
+++ api/resources/market.py
@@ -1,19 +1,40 @@
 # TODO: Only require market stats that are being used by ML models
 # TODO: Allow storage/retrieval of multiple markets
 from api import api, db
+from api.helpers import HTTP_CODES, query_to_dict, validate_db
+from api.models.market import Data as DataModel
+from flask import abort
 from flask_restful import Resource
-from api.models.market import Data
 from webargs import fields
 from webargs.flaskparser import use_kwargs
 
 @api.resource('/api/private/market/')
 class Data(Resource):
-    """ Market data of an instance in time """
-    def get(self):
-        return {'foo': 'bar'}
+    """ Market data at an instance in time """
+    @use_kwargs({
+        'id': fields.Integer(required=True)
+    })
+    @validate_db(db)
+    def get(self, id):
+        return query_to_dict(DataModel.query.get_or_404(id))
 
-    def post(self):
-        return {'foo': 'bar'}
+    @use_kwargs({
+        'id': fields.Integer(required=True),
+        'low': fields.Float(missing=None),
+        'high': fields.Float(missing=None),
+        'close': fields.Float(missing=None),
+        'volume': fields.Float(missing=None)
+    })
+    @validate_db(db)
+    def post(self, id, low, high, close, volume):
+        try:
+            post_request = DataModel(id, low, high, close, volume)
+            db.session.add(post_request)
+            db.session.commit()
+        except: # ID already exists, use PUT
+            abort(HTTP_CODES.UNPROCESSABLE_ENTITY)
+        else:
+            return query_to_dict(post_request)
 
     def put(self):
         return {'foo': 'bar'}
