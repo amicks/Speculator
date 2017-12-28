e74aff778d6657a8c4993c62f264008c9be99e78
Allston Mickey <allston.mickey@gmail.com>
2017-12-28 14:16:23 -0500

    Fix validate DB when DB is disabled and not connected

--- api/app.py
+++ api/app.py
@@ -1,5 +1,5 @@
 # TODO: Add private API with Redis Cache and PostgreSQL (or any SQL DB with SQLAlchemy)
-from api import api, cache, db
+from api import api, cache, ENABLE_DB, db
 from flask import abort, Flask
 from flask_restful import Resource
 from os import getenv
@@ -7,12 +7,15 @@ from api.resources.market import Data
 from api.resources.trend import Predict
 
 def setup_app():
-    db_uri = getenv('SQLALCHEMY_DATABASE_URI') # format: postgresql://user:pw@host:port/db
-    if not db_uri:
-        abort(401)
-
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+
+    if ENABLE_DB:
+        db_uri = getenv('SQLALCHEMY_DATABASE_URI') # format: postgresql://user:pw@host:port/db
+        if db_uri:
+            app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+        else:
+            abort(401)
+    
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     with app.app_context():
         api.init_app(app)
@@ -29,7 +32,8 @@ def setup_db(application, sqlalchemy_bind, mem_cache=None):
 if __name__=='__main__':
     app = setup_app()
 
-    from api.models import * # Load all DB models
-    setup_db(app, db, mem_cache=cache)
+    if ENABLE_DB:
+        from api.models import * # Load all DB models
+        setup_db(app, db, mem_cache=cache)
 
     app.run(debug=True)
